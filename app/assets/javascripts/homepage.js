720c1e80ad59fe4eca57e8fb3b5751281c2c2e43
Nick Chen <ska4908@gmail.com>
2015-07-29 19:43:48 +0800

    fix owl carousel's bug

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,12 +1,12 @@
 
 $(document).on('ready page:load', function() {
   // fix owl carousel's bug
-  $('.owl-carousel').append('<div class="unit item"></div>');
-  
+  //$('.owl-carousel').append('<div class="unit item"></div>');
+
   $('.owl-carousel').owlCarousel({
     margin:5,
-    autoWidth:true,
-    items: 4
+    autoWidth:true
+
   });
 
 });
